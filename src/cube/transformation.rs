88a56cf7487872d6caa0ed55450e7fc0617af297
Divino Gervasio de Menezes Neto <dgmneto@gmail.com>
2020-04-05 13:44:56 +0100

    Test all transformations are correct

--- src/cube/transformation.rs
+++ src/cube/transformation.rs
@@ -1,7 +1,7 @@
 use super::cube::{Coordinate, CubeState};
 use std::collections::HashMap;
 
-#[derive(PartialEq,Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Transformation {
     moves: HashMap<Coordinate, Coordinate>,
 }
@@ -26,13 +26,10 @@ impl Transformation {
         let mut new_face = [[0u8; 3]; 3];
         for i in 0..3 {
             for j in 0..3 {
-                let coordinate = (face, i, j);
-                new_face[i][j] = if self.moves.contains_key(&coordinate) {
-                    let (face, i, j) = self.moves[&coordinate];
-                    state[face][i][j]
-                } else {
-                    state[face][i][j]
-                }
+                let new_coordinate = (face, i, j);
+                let &(old_face, old_i, old_j) =
+                    self.moves.get(&new_coordinate).unwrap_or(&new_coordinate);
+                new_face[i][j] = state[old_face][old_i][old_j];
             }
         }
         new_face
@@ -42,6 +39,7 @@ impl Transformation {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
 
     #[test]
     fn no_two_transformation_are_equal() {
@@ -59,5 +57,51 @@ mod tests {
     fn exactly_12_transformations() {
         let transformations = Transformation::all_transformations();
         assert_eq!(transformations.len(), 12);
+    }
+
+    #[test]
+    fn all_transformations_are_correct() {
+        let transformations = Transformation::all_transformations();
+        let state = checkable_state();
+        for transformation in transformations {
+            let new_state = transformation.apply(&state);
+            assert_transfomation_changes(&transformation, &state, new_state);
+        }
+    }
+
+    fn checkable_state() -> CubeState {
+        let mut state = [[[0u8; 3]; 3]; 6];
+        let mut val = 0u8;
+        for face in 0..6 {
+            for i in 0..3 {
+                for j in 0..3 {
+                    state[face][i][j] = val;
+                    val += 1;
+                }
+            }
+        }
+        state
+    }
+
+    fn assert_transfomation_changes(
+        transformation: &Transformation,
+        old_state: &CubeState,
+        new_state: CubeState,
+    ) {
+        let mut new_state_values = HashSet::new();
+        for face in 0..6 {
+            for i in 0..3 {
+                for j in 0..3 {
+                    let new_coordinate = (face, i, j);
+                    let &(old_face, old_i, old_j) = transformation
+                        .moves
+                        .get(&new_coordinate)
+                        .unwrap_or(&new_coordinate);
+                    assert_eq!(old_state[old_face][old_i][old_j], new_state[face][i][j]);
+                    new_state_values.insert(new_state[face][i][j]);
+                }
+            }
+        }
+        assert_eq!(new_state_values.len(), 54);
     }
 }
